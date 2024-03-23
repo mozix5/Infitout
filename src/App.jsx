82677cb49cf2311654e9e62df219d22cdc99ca99
mozix5 <mosinmd827@gmail.com>
2024-03-23 18:58:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SimpleBarChart from './components/SimpleBarChart'
+
+const App = () => {
+  return (
+    <div className=' '>App
+    <SimpleBarChart/>
+    </div>
+  )
+}
+
+export default App
