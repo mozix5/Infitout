d6c1b604e0bf5715f96a5d2d67c22192da29b08f
mozix5 <mosinmd827@gmail.com>
2024-03-25 02:47:21 +0530

    added mock data

---
+++ src/assets/data.js
@@ -0,0 +1,851 @@
+export const data = [
+  {
+    time_series_5min: {
+      summary: {
+        Bearish: 14,
+        Neutral: 8,
+        Bullish: 5,
+        "EMA (20)": 22108.15,
+        "SMA(20)": 22115.62,
+        "RSI(14)": 40.38,
+        "Awesome Osc.": -18.79,
+        "Macd (12, 26, 9)": -1.74,
+        "CCI (20)": -97.47,
+      },
+      support_resistance: {
+        support: {
+          S1: 21943.62,
+          S2: 21872.63,
+          S3: 21732.98,
+        },
+        resistance: {
+          R1: 22083.27,
+          R2: 22151.93,
+          R3: 22291.58,
+        },
+      },
+      moving_averages: {
+        "EMA (5)": {
+          value: 22091.28,
+          position: "S",
+        },
+        "SMA (5)": {
+          value: 22087.02,
+          position: "S",
+        },
+        "EMA (10)": {
+          value: 22102.87,
+          position: "S",
+        },
+        "SMA (10)": {
+          value: 22114.63,
+          position: "S",
+        },
+        "EMA (20)": {
+          value: 22108.15,
+          position: "S",
+        },
+        "SMA (20)": {
+          value: 22115.62,
+          position: "S",
+        },
+        "EMA (30)": {
+          value: 22105.43,
+          position: "S",
+        },
+        "SMA (30)": {
+          value: 22110.67,
+          position: "S",
+        },
+        "EMA (50)": {
+          value: 22094.2,
+          position: "S",
+        },
+        "SMA (50)": {
+          value: 22100.39,
+          position: "S",
+        },
+        "EMA (100)": {
+          value: 22063.52,
+          position: "B",
+        },
+        "SMA (100)": {
+          value: 22055.74,
+          position: "B",
+        },
+        "EMA (200)": {
+          value: 22022.45,
+          position: "B",
+        },
+        "SMA (200)": {
+          value: 21997.33,
+          position: "B",
+        },
+        "Ichimoku Base line (9, 26, 52, 26)": {
+          value: 22122.25,
+          position: "N",
+        },
+        "Volume Weighted MA (20)": {
+          value: 0.0,
+          position: "N",
+        },
+        "Hull MA (9)": {
+          value: 22071.08,
+          position: "B",
+        },
+      },
+      Oscillators: {
+        Bearish: 4,
+        Neutral: 6,
+        Bullish: 0,
+        "RSI (14)": {
+          value: 40.38,
+          position: "N",
+        },
+        "Stoch.%K (14, 3, 3)": {
+          value: 14.54,
+          position: "N",
+        },
+        "CCI (20)": {
+          value: -97.47,
+          position: "N",
+        },
+        "ADX (14)": {
+          value: 26.67,
+          position: "S",
+        },
+        "Awesome Osc.": {
+          value: -18.79,
+          position: "S",
+        },
+        "Momentum (10)": {
+          value: -52.2,
+          position: "S",
+        },
+        "Macd (12, 26, 9)": {
+          value: -1.74,
+          position: "N",
+        },
+        "Stoch. RSI Fast (3, 3, 14, 14)": {
+          value: 8.39,
+          position: "S",
+        },
+        "Williams %Range (14)": {
+          value: -85.46,
+          position: "N",
+        },
+        "Ultimate Osc. (7, 14, 28)": {
+          value: 40.59,
+          position: "N",
+        },
+      },
+    },
+  },
+  {
+    time_series_10minutes: {
+      summary: {
+        Bearish: 11,
+        Neutral: 9,
+        Bullish: 7,
+        "EMA (20)": 22098.53,
+        "SMA(20)": 22104.85,
+        "RSI(14)": 46.95,
+        "Awesome Osc.": 25.6,
+        "Macd (12, 26, 9)": 11.71,
+        "CCI (20)": -79.31,
+      },
+      support_resistance: {
+        support: {
+          S1: 21943.62,
+          S2: 21872.63,
+          S3: 21732.98,
+        },
+        resistance: {
+          R1: 22083.27,
+          R2: 22151.93,
+          R3: 22291.58,
+        },
+      },
+      moving_averages: {
+        Bearish: 8,
+        Neutral: 2,
+        Bullish: 7,
+        Indicators: {
+          "EMA (5)": {
+            position: "S",
+            value: 22096.01,
+          },
+          "SMA (5)": {
+            position: "S",
+            value: 22102.78,
+          },
+          "EMA (10)": {
+            position: "S",
+            value: 22103.63,
+          },
+          "SMA (10)": {
+            position: "S",
+            value: 22113.59,
+          },
+          "EMA (20)": {
+            position: "S",
+            value: 22098.53,
+          },
+          "SMA (20)": {
+            position: "S",
+            value: 22104.85,
+          },
+          "EMA (30)": {
+            position: "S",
+            value: 22087.47,
+          },
+          "SMA (30)": {
+            position: "S",
+            value: 22092.96,
+          },
+          "EMA (50)": {
+            position: "B",
+            value: 22064.03,
+          },
+          "SMA (50)": {
+            position: "B",
+            value: 22057.15,
+          },
+          "EMA (100)": {
+            position: "B",
+            value: 22023.01,
+          },
+          "SMA (100)": {
+            position: "B",
+            value: 22000.62,
+          },
+          "EMA (200)": {
+            position: "B",
+            value: 22015.68,
+          },
+          "SMA (200)": {
+            position: "B",
+            value: 21966.17,
+          },
+          "Ichimoku Base line (9, 26, 52, 26)": {
+            position: "N",
+            value: 22115.08,
+          },
+          "Volume Weighted MA (20)": {
+            position: "N",
+            value: 0.0,
+          },
+          "Hull MA (9)": {
+            position: "B",
+            value: 22076.15,
+          },
+        },
+      },
+      Oscillators: {
+        Bearish: 3,
+        Neutral: 7,
+        Bullish: 0,
+        Indicators: {
+          "RSI (14)": {
+            position: "N",
+            value: 46.95,
+          },
+          "Stoch.%K (14, 3, 3)": {
+            position: "N",
+            value: 14.54,
+          },
+          "CCI (20)": {
+            position: "N",
+            value: -79.31,
+          },
+          "ADX (14)": {
+            position: "N",
+            value: 18.59,
+          },
+          "Awesome Osc.": {
+            position: "N",
+            value: 25.6,
+          },
+          "Momentum (10)": {
+            position: "S",
+            value: -13.35,
+          },
+          "Macd (12, 26, 9)": {
+            position: "S",
+            value: 11.71,
+          },
+          "Stoch. RSI Fast (3, 3, 14, 14)": {
+            position: "N",
+            value: 0.97,
+          },
+          "Williams %Range (14)": {
+            position: "S",
+            value: -85.46,
+          },
+          "Ultimate Osc. (7, 14, 28)": {
+            position: "N",
+            value: 39.97,
+          },
+        },
+      },
+    },
+  },
+  {
+    time_series_15minutes: {
+      summary: {
+        Bearish: 9,
+        Neutral: 9,
+        Bullish: 9,
+        Indicators: {
+          "EMA (20)": 22086.1,
+          "SMA(20)": 22090.76,
+          "RSI(14)": 51.51,
+          "Awesome Osc.": 67.3,
+          "Macd (12, 26, 9)": 24.23,
+          "CCI (20)": -32.52,
+        },
+      },
+      support_resistance: {
+        S1: 21943.62,
+        S2: 21872.63,
+        S3: 21732.98,
+        R1: 22083.27,
+        R2: 22151.93,
+        R3: 22291.58,
+      },
+      moving_averages: {
+        Bearish: 7,
+        Neutral: 2,
+        Bullish: 8,
+        Indicators: {
+          "EMA (5)": {
+            position: "S",
+            value: 22101.14,
+          },
+          "SMA (5)": {
+            position: "S",
+            value: 22116.6,
+          },
+          "EMA (10)": {
+            position: "S",
+            value: 22101.86,
+          },
+          "SMA (10)": {
+            position: "S",
+            value: 22108.73,
+          },
+          "EMA (20)": {
+            position: "S",
+            value: 22086.1,
+          },
+          "SMA (20)": {
+            position: "S",
+            value: 22090.76,
+          },
+          "EMA (30)": {
+            position: "B",
+            value: 22068.1,
+          },
+          "SMA (30)": {
+            position: "B",
+            value: 22060.84,
+          },
+          "EMA (50)": {
+            position: "B",
+            value: 22037.81,
+          },
+          "SMA (50)": {
+            position: "B",
+            value: 22043.75,
+          },
+          "EMA (100)": {
+            position: "B",
+            value: 22010.37,
+          },
+          "SMA (100)": {
+            position: "B",
+            value: 21945.47,
+          },
+          "EMA (200)": {
+            position: "B",
+            value: 22040.68,
+          },
+          "SMA (200)": {
+            position: "B",
+            value: 22006.64,
+          },
+          "Ichimoku Base line (9, 26, 52, 26)": {
+            position: "N",
+            value: 22032.0,
+          },
+          "Volume Weighted MA (20)": {
+            position: "N",
+            value: 0.0,
+          },
+          "Hull MA (9)": {
+            position: "S",
+            value: 22107.19,
+          },
+        },
+      },
+      Oscillators: {
+        Bearish: 2,
+        Neutral: 7,
+        Bullish: 1,
+        Indicators: {
+          "RSI (14)": {
+            position: "N",
+            value: 51.51,
+          },
+          "Stoch.%K (14, 3, 3)": {
+            position: "N",
+            value: 16.92,
+          },
+          "CCI (20)": {
+            position: "N",
+            value: -32.52,
+          },
+          "ADX (14)": {
+            position: "N",
+            value: 13.57,
+          },
+          "Awesome Osc.": {
+            position: "N",
+            value: 67.3,
+          },
+          "Momentum (10)": {
+            position: "S",
+            value: -15.7,
+          },
+          "Macd (12, 26, 9)": {
+            position: "S",
+            value: 24.23,
+          },
+          "Stoch. RSI Fast (3, 3, 14, 14)": {
+            position: "N",
+            value: 12.81,
+          },
+          "Williams %Range (14)": {
+            position: "B",
+            value: -83.08,
+          },
+          "Ultimate Osc. (7, 14, 28)": {
+            position: "N",
+            value: 45.33,
+          },
+        },
+      },
+    },
+  },
+  {
+    time_series_30minutes: {
+      summary: {
+        Bearish: 7,
+        Neutral: 12,
+        Bullish: 8,
+        Indicators: {
+          "EMA (20)": 22053.62,
+          "SMA(20)": 22051.08,
+          "RSI(14)": 105.57,
+          "Awesome Osc.": 39.81,
+          "Macd (12, 26, 9)": 51.64,
+          "CCI (20)": null,
+        },
+      },
+      support_resistance: {
+        S1: 21759.13,
+        S2: 21521.92,
+        S3: 20900.97,
+        R1: 22380.08,
+        R2: 22763.82,
+        R3: 23384.77,
+      },
+      moving_averages: {
+        Bearish: 7,
+        Neutral: 2,
+        Bullish: 8,
+        Indicators: {
+          "EMA (5)": {
+            position: "S",
+            value: 22094.01,
+          },
+          "SMA (5)": {
+            position: "S",
+            value: 22101.84,
+          },
+          "EMA (10)": {
+            position: "S",
+            value: 22083.78,
+          },
+          "SMA (10)": {
+            position: "S",
+            value: 22091.39,
+          },
+          "EMA (20)": {
+            position: "B",
+            value: 22053.62,
+          },
+          "SMA (20)": {
+            position: "B",
+            value: 22051.08,
+          },
+          "EMA (30)": {
+            position: "B",
+            value: 22029.79,
+          },
+          "SMA (30)": {
+            position: "B",
+            value: 22020.26,
+          },
+          "EMA (50)": {
+            position: "B",
+            value: 22011.07,
+          },
+          "SMA (50)": {
+            position: "B",
+            value: 21946.93,
+          },
+          "EMA (100)": {
+            position: "B",
+            value: 22037.56,
+          },
+          "SMA (100)": {
+            position: "B",
+            value: 21992.88,
+          },
+          "EMA (200)": {
+            position: "S",
+            value: 22081.9,
+          },
+          "SMA (200)": {
+            position: "S",
+            value: 22185.05,
+          },
+          "Ichimoku Base line (9, 26, 52, 26)": {
+            position: "N",
+            value: 22032.0,
+          },
+          "Volume Weighted MA (20)": {
+            position: "N",
+            value: 0.0,
+          },
+          "Hull MA (9)": {
+            position: "S",
+            value: 22109.27,
+          },
+        },
+      },
+      Oscillators: {
+        Bearish: 0,
+        Neutral: 10,
+        Bullish: 0,
+        Indicators: {
+          "RSI (14)": {
+            position: "N",
+            value: 55.85,
+          },
+          "Stoch.%K (14, 3, 3)": {
+            position: "N",
+            value: 66.41,
+          },
+          "CCI (20)": {
+            position: "N",
+            value: 51.64,
+          },
+          "ADX (14)": {
+            position: "N",
+            value: 12.79,
+          },
+          "Awesome Osc.": {
+            position: "N",
+            value: 105.57,
+          },
+          "Momentum (10)": {
+            position: "N",
+            value: 44.35,
+          },
+          "Macd (12, 26, 9)": {
+            position: "N",
+            value: 39.81,
+          },
+          "Stoch. RSI Fast (3, 3, 14, 14)": {
+            position: "N",
+            value: 46.62,
+          },
+          "Williams %Range (14)": {
+            position: "N",
+            value: -33.59,
+          },
+          "Ultimate Osc. (7, 14, 28)": {
+            position: "N",
+            value: 51.01,
+          },
+        },
+      },
+    },
+  },
+  {
+    time_series_hour: {
+      summary: {
+        Bearish: 4,
+        Neutral: 10,
+        Bullish: 13,
+        Indicators: {
+          "EMA (20)": 22017.77,
+          "SMA(20)": 21987.83,
+          "RSI(14)": 58.31,
+          "Awesome Osc.": 133.74,
+          "Macd (12, 26, 9)": 35.51,
+          "CCI (20)": 76.55,
+        },
+      },
+      support_resistance: {
+        Support: {
+          S1: 21759.13,
+          S2: 21521.92,
+          S3: 20900.97,
+        },
+        Resistance: {
+          R1: 22380.08,
+          R2: 22763.82,
+          R3: 23384.77,
+        },
+      },
+      moving_averages: {
+        Bearish: 0,
+        Neutral: 10,
+        Bullish: 0,
+        "RSI (14)": {
+          position: "N",
+          value: 55.85,
+        },
+        "Stoch.%K (14, 3, 3)": {
+          position: "N",
+          value: 66.41,
+        },
+        "CCI (20)": {
+          position: "N",
+          value: 51.64,
+        },
+        "ADX (14)": {
+          position: "N",
+          value: 12.79,
+        },
+        "Awesome Osc.": {
+          position: "N",
+          value: 105.57,
+        },
+        "Momentum (10)": {
+          position: "N",
+          value: 44.35,
+        },
+        "Macd (12, 26, 9)": {
+          position: "N",
+          value: 39.81,
+        },
+        "Stoch. RSI Fast (3, 3, 14, 14)": {
+          position: "N",
+          value: 46.62,
+        },
+        "Williams %Range (14)": {
+          position: "N",
+          value: -33.59,
+        },
+        "Ultimate Osc. (7, 14, 28)": {
+          position: "N",
+          value: 51.01,
+        },
+      },
+      Oscillators: {
+        Bearish: 0,
+        Neutral: 8,
+        Bullish: 2,
+        "RSI (14)": {
+          position: "N",
+          value: 58.31,
+        },
+        "Stoch.%K (14, 3, 3)": {
+          position: "N",
+          value: 66.41,
+        },
+        "CCI (20)": {
+          position: "N",
+          value: 76.55,
+        },
+        "ADX (14)": {
+          position: "N",
+          value: 16.02,
+        },
+        "Awesome Osc.": {
+          position: "B",
+          value: 133.74,
+        },
+        "Momentum (10)": {
+          position: "N",
+          value: 77.2,
+        },
+        "Macd (12, 26, 9)": {
+          position: "B",
+          value: 35.51,
+        },
+        "Stoch. RSI Fast (3, 3, 14, 14)": {
+          position: "N",
+          value: 60.48,
+        },
+        "Williams %Range (14)": {
+          position: "N",
+          value: -33.59,
+        },
+        "Ultimate Osc. (7, 14, 28)": {
+          position: "N",
+          value: 59.8,
+        },
+      },
+    },
+  },
+  {
+    time_series_one_day: {
+      summary: {
+        Bearish: 3,
+        Neutral: 10,
+        Bullish: 14,
+        "RSI (14)": -96.92,
+        "Stoch.%K (14, 3, 3)": 51.48,
+        "CCI (20)": -43.9,
+        "ADX (14)": 0.0,
+        "Awesome Osc.": 14.42,
+        "Momentum (10)": 0.0,
+        "Macd (12, 26, 9)": -43.9,
+        "Stoch. RSI Fast (3, 3, 14, 14)": 0.0,
+        "Williams %Range (14)": 0.0,
+        "Ultimate Osc. (7, 14, 28)": 0.0,
+      },
+      support_resistance: {
+        S1: 21576.17,
+        S2: 21169.53,
+        S3: 20402.23,
+        R1: 22343.47,
+        R2: 22704.13,
+        R3: 23471.43,
+      },
+      moving_averages: {
+        Bearish: 2,
+        Neutral: 1,
+        Bullish: 14,
+        "EMA (5)": {
+          position: "B",
+          value: 22016.03,
+        },
+        "SMA (5)": {
+          position: "B",
+          value: 21964.19,
+        },
+        "EMA (10)": {
+          position: "B",
+          value: 22056.57,
+        },
+        "SMA (10)": {
+          position: "B",
+          value: 22065.7,
+        },
+        "EMA (20)": {
+          position: "B",
+          value: 22076.49,
+        },
+        "SMA (20)": {
+          position: "S",
+          value: 22159.61,
+        },
+        "EMA (30)": {
+          position: "B",
+          value: 22031.16,
+        },
+        "SMA (30)": {
+          position: "B",
+          value: 22090.58,
+        },
+        "EMA (50)": {
+          position: "B",
+          value: 21857.6,
+        },
+        "SMA (50)": {
+          position: "B",
+          value: 21931.91,
+        },
+        "EMA (100)": {
+          position: "B",
+          value: 21332.67,
+        },
+        "SMA (100)": {
+          position: "B",
+          value: 21205.93,
+        },
+        "EMA (200)": {
+          position: "B",
+          value: 20486.04,
+        },
+        "SMA (200)": {
+          position: "B",
+          value: 20312.01,
+        },
+        "Ichimoku Base line (9, 26, 52, 26)": {
+          position: "B",
+          value: 22118.4,
+        },
+        "Volume Weighted MA (20)": {
+          position: "N",
+          value: 0.0,
+        },
+        "Hull MA (9)": {
+          position: "B",
+          value: 21909.5,
+        },
+      },
+      Oscillators: {
+        Bearish: 1,
+        Neutral: 9,
+        Bullish: 0,
+        "RSI (14)": {
+          position: "N",
+          value: 51.48,
+        },
+        "Stoch.%K (14, 3, 3)": {
+          position: "N",
+          value: 47.35,
+        },
+        "CCI (20)": {
+          position: "N",
+          value: -43.9,
+        },
+        "ADX (14)": {
+          position: "N",
+          value: 16.19,
+        },
+        "Awesome Osc.": {
+          position: "S",
+          value: -96.92,
+        },
+        "Momentum (10)": {
+          position: "N",
+          value: -396.8,
+        },
+        "Macd (12, 26, 9)": {
+          position: "N",
+          value: 14.42,
+        },
+        "Stoch. RSI Fast (3, 3, 14, 14)": {
+          position: "N",
+          value: 10.72,
+        },
+        "Williams %Range (14)": {
+          position: "N",
+          value: -52.65,
+        },
+        "Ultimate Osc. (7, 14, 28)": {
+          position: "N",
+          value: 55.75,
+        },
+      },
+    },
+  },
+];
